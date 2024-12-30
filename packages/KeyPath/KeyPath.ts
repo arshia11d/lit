b90728e8aa66a0fc27d22a702de43ba37e81b715
a11delavar <a11delavar@outlook.com>
2024-12-31 00:12:21 +0100

    Proposal

--- packages/KeyPath/KeyPath.ts
+++ packages/KeyPath/KeyPath.ts
@@ -1,39 +1,37 @@
 import '@a11d/is-writable'
 
-export function getKeyPath<T>(keyPath: KeyPathOf<T>): KeyPathOf<T> {
-	return keyPath
-}
+export class KeyPath {
+	static of<T>(keyPath: KeyPathOf<T>): KeyPathOf<T> {
+		return keyPath
+	}
 
-export function getValueByKeyPath<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath): KeyPathValueOf<T, KeyPath> {
-	return !keyPath ? undefined : keyPath
-		.split('.')
-		.reduce((value: any, key) => value === undefined || value === null ? value : value[key], object)
-}
+	static get<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath): KeyPathValueOf<T, KeyPath> {
+		return !keyPath ? undefined : keyPath
+			.split('.')
+			.reduce((value: any, key) => value === undefined || value === null ? value : value[key], object)
+	}
 
-export function setValueByKeyPath<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath, value: KeyPathValueOf<T, KeyPath>) {
-	const keys = keyPath.split('.')
-	const lastKey = keys[keys.length - 1] as keyof T
-	const otherKeysButLast = keys.slice(0, keys.length - 1)
-	const lastObject = getValueByKeyPath(object, otherKeysButLast.join('.') as KeyPath) ?? object as any
-	if (Object.isWritable(lastObject, lastKey)) {
-		lastObject[lastKey] = value
+	static set<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath, value: KeyPathValueOf<T, KeyPath>) {
+		const keys = keyPath.split('.')
+		const lastKey = keys[keys.length - 1] as keyof T
+		const otherKeysButLast = keys.slice(0, keys.length - 1)
+		const lastObject = KeyPath.get(object, otherKeysButLast.join('.') as KeyPath) ?? object as any
+		if (Object.isWritable(lastObject, lastKey)) {
+			lastObject[lastKey] = value
+		}
+	}
+
+	static isWritable<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath): boolean {
+		if (!keyPath) return false
+		const keys = keyPath.split('.')
+		const lastKey = keys[keys.length - 1] as keyof T
+		const otherKeysButLast = keys.slice(0, keys.length - 1)
+		const lastObject = KeyPath.get(object, otherKeysButLast.join('.') as KeyPath) ?? object as any
+		return Object.isWritable(lastObject, lastKey)
 	}
 }
 
-export function isKeyPathWritable<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath): boolean {
-	if (!keyPath) return false
-	const keys = keyPath.split('.')
-	const lastKey = keys[keys.length - 1] as keyof T
-	const otherKeysButLast = keys.slice(0, keys.length - 1)
-	const lastObject = getValueByKeyPath(object, otherKeysButLast.join('.') as KeyPath) ?? object as any
-	return Object.isWritable(lastObject, lastKey)
-}
-
-// @ts-expect-error = TypeScript deconstructs the types if we get it's type
-globalThis.getKeyPath = getKeyPath
-globalThis.getValueByKeyPath = getValueByKeyPath
-globalThis.setValueByKeyPath = setValueByKeyPath
-globalThis.isKeyPathWritable = isKeyPathWritable
+globalThis.KeyPath = KeyPath
 
 type DepthLevels = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 type DefaultDepth = 1
@@ -41,23 +39,22 @@ type ShouldBreak<Deep extends DepthLevels[number]> = [Deep] extends [never] ? tr
 type NextCycle<Deep extends DepthLevels[number]> = DepthLevels[Deep]
 
 declare global {
-	function getKeyPath<T>(keyPath: KeyPathOf<T>): KeyPathOf<T>
-	function getValueByKeyPath<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath): KeyPathValueOf<T, KeyPath>
-	function setValueByKeyPath<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath, value: KeyPathValueOf<T, KeyPath>): void
-	function isKeyPathWritable<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath): boolean
+	// eslint-disable-next-line no-var
+	var KeyPath: typeof import('./KeyPath.js').KeyPath
 
 	type KeyPathOf<T, Depth extends DepthLevels[number] = DefaultDepth> =
-		ShouldBreak<Depth> extends true ? never :
-		object extends Required<T> ? string :
-		T extends ReadonlyArray<any> ? Extract<keyof T, `${number}`> | Extract<keyof T, string> | SubKeyPathOf<T, Extract<keyof T, `${number}`>, Depth> :
-		T extends object ? Extract<keyof T, string> | SubKeyPathOf<T, Extract<keyof T, string>, Depth> :
-		never
+		ShouldBreak<Depth> extends true ? never
+		: object extends Required<T> ? string
+		: T extends ReadonlyArray<any> ? Extract<keyof T, `${number}`> | Extract<keyof T, string> | SubKeyPathOf<T, Extract<keyof T, `${number}`>, Depth>
+		: T extends object ? Extract<keyof T, string> | SubKeyPathOf<T, Extract<keyof T, string>, Depth>
+		: never
 
 	type KeyPathValueOf<T, KeyPath extends string = KeyPathOf<T>, Depth extends DepthLevels[number] = DefaultDepth> =
-		ShouldBreak<Depth> extends true ? never :
-		KeyPath extends keyof T ? T[KeyPath] :
-		KeyPath extends `${infer K}.${infer R}` ? K extends keyof T ? KeyPathValueOf<T[K], R, Depth> : unknown :
-		unknown
+		ShouldBreak<Depth> extends true ? never
+		: KeyPath extends keyof T ? T[KeyPath]
+		: KeyPath extends `${infer K}.${infer R}` ? K extends keyof T ? KeyPathValueOf<T[K], R, Depth> : unknown
+		: unknown
+
 }
 
 type SubKeyPathOf<T, K extends string, Depth extends DepthLevels[number]> = K extends keyof T ? `${K}.${KeyPathOf<T[K], NextCycle<Depth>>}` : never
